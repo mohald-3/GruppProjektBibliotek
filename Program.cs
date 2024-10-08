bdfc207319fb5bbc58dfa06b40056c8d0dd40115
Mohanad Al-Daghestani <mohanad.aldaghestani@gmail.com>
2024-10-08 13:34:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GruppProjektBibliotek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej grupp 2! Vi gör projektet tillsammans!");
+        }
+    }
+}
