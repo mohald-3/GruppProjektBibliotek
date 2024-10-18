cfed954bb6dfb49d6a6fdcfa9be3a8c6396d8ef9
bothens <alexanderbothen@icloud.com>
2024-10-18 15:21:42 +0200

    Gjorde en raderabok  metod

--- Bibliotek.cs
+++ Bibliotek.cs
@@ -1,4 +1,7 @@
-﻿namespace GruppProjektBibliotek
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GruppProjektBibliotek
 {
     public class Bibliotek
     {
@@ -9,8 +12,23 @@
             Böcker = new List<Bok>();
 
         }
-            
-        
 
+
+        public void TakeAwayBook()
+        {
+            Console.Write("Skriv titeln på boken du vill radera: ");
+            string titel = Console.ReadLine()!;
+
+            var bok = Böcker.Find(b => b.BokTitel.Equals(titel, StringComparison.OrdinalIgnoreCase));
+
+            if (bok == null)
+            {
+                Console.WriteLine($"Boken \"{titel}\" kunde tyvärr inte hittas.");
+                return;
+            }
+
+            Böcker.Remove(bok);
+            Console.WriteLine($"Boken \"{titel}\" har tagits bort från biblioteket.");
+        }
     }
-}
+    }
