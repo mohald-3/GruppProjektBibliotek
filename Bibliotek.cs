f11e8f9c4f96ba8b9dffec91cd720528b955a27c
bothens <alexanderbothen@icloud.com>
2024-10-19 17:03:42 +0200

    ny pull request

--- Bibliotek.cs
+++ Bibliotek.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.Design;
+﻿
 using System.Security.Cryptography.X509Certificates;
 
 namespace GruppProjektBibliotek
--- Bok.cs
+++ Bok.cs
@@ -2,7 +2,7 @@
 {
     public class Bok
     {
-        public List<Bok> Böcker { get; set; } = new List<Bok>();
+       
         public string BokTitel { get; set; }
         public string BokFörfattare { get; set; }
         public int ISBN { get; set; }
